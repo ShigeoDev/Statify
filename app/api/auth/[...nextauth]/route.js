72fe90c0f276e5d8e3397ed1eea866c32625bfe0
ShigeoDev <84653162+ShigeoDev@users.noreply.github.com>
2024-08-08 13:09:47 -0700

    using .js for route

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,4 +1,4 @@
-import NextAuth, { NextAuthOptions } from "next-auth"
+import NextAuth from "next-auth"
 import SpotifyProvider from "next-auth/providers/spotify"
 
 const scopes = [
@@ -18,8 +18,29 @@ const params = new URLSearchParams({
 
 const LOGIN_URL = `https://accounts.spotify.com/authorize?${params.toString()}`
 
+async function refreshAccessToken(token) {
+  const params = new URLSearchParams()
+  params.append("grant_type", "refresh_token")
+  params.append("refresh_token", token.refreshToken)
+  const response = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer.from(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_SECRET).toString('base64'))
+    },
+    body: params
+  })
+  const data = await response.json()
+  return {
+    ...token,
+    accessToken: data.access_token,
+    refreshToken: data.refresh_token ?? token.refreshToken,
+    accessTokenExpires: Date.now() + data.expires_in * 1000
+  }
+}
 
-const authOptions: NextAuthOptions = {
+
+
+const authOptions = {
   providers: [
     SpotifyProvider({
       clientId: process.env.SPOTIFY_CLIENT_ID ?? "",
@@ -38,11 +59,17 @@ const authOptions: NextAuthOptions = {
         token.accessToken = account.access_token
         token.refreshToken = account.refresh_token
         token.accessTokenExpires = account.expires_at
+        return token
       }
-      return token
+      // access token has not expired
+      if (token.accessTokenExpires && Date.now() < token.accessTokenExpires * 1000) {
+        return token
+      }
 
+      // access token has expired
+      return await refreshAccessToken(token)
     },
-    async session({ session, token }) {
+    async session({ session, token, user }) {
       // Send properties to the client, like an access_token from a provider.
       session.accessToken = token.accessToken
       return session
